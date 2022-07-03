718ff8badc23dbb6d10d44a4d36de0ecda87681e
Naganishe <mr.nordakov@mail.ru>
2022-07-03 19:26:48 +0300

    add C#HW1

---
+++ C#HW1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте двумерный массив размером M x N,
+// Заполненный случайными вещественными числами.
+
+double [,] DoubleArray (int row, int col)
+{
+    double [,] array = new double [row, col];
+    return array;
+}
+
+void FillArray (double [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix [i, j] = (new Random().NextDouble()) * (new Random().Next (-9, 9));
+        }
+    }
+}
+
+void PrintArray (double [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+Console.WriteLine($"Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите количество столбцов: ");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+double [,] matrix = DoubleArray(row, col);
+
+FillArray(matrix);
+PrintArray(matrix);
